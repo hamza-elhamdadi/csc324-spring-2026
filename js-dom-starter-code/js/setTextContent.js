f33c42bc7b36d866a0488df17818f23241a6d389
Elhamdadi <helhamdadistonehill.edu@Mac.lan>
2026-03-03 10:20:02 -0500

    cleaned up the js-dom-starter-code

--- js-dom-starter-code/js/setTextContent.js
+++ js-dom-starter-code/js/setTextContent.js
@@ -1,11 +1,11 @@
-const setParagraphText = function(text) {
+const setParagraphText = function() {
     // TODO: set the text of the first paragraph
     // TODO: set the text of all of the paragraphs
     // TODO: set the text of the third paragraph
     
 }
 
-const createNewParagraph = function(text) {
+const createNewParagraph = function() {
     // TODO: create a new paragraph and add it to the <main>
 }
 
