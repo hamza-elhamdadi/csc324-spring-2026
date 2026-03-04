69670e9fa839a1f959a005f2ceb421baeac0e2f1
Elhamdadi <helhamdadistonehill.edu@F-HELHAMDADI.local>
2026-03-04 14:06:24 -0500

    make-a-post front-end starter code added

---
+++ make-a-post/js/posts.js
@@ -0,0 +1,25 @@
+function addPost(post) {
+    // TODO: Select the div with the id "post-container"
+    // TODO: Make the object "post" a child of the post-container div
+}
+
+function createPost(postText, profilePicImagePath) {
+    // TODO: create a post like the example post using the JavaScript DOM.
+    // Note: to create an element use document.createElement("tag-name")
+    //       to add a class to an element use .classList.add()
+    //       to set an attribute (e.g., 'src'), .attribute = "valueToSet"
+    //       to make one element a child of another, use parentElement.appendChild(childElement)
+
+}
+
+function postHandler(event){
+    // TODO: get the input value and call createPost and addPost above
+    
+}
+
+function setup() {
+    // TODO: Select the form with id="add-post" and add a "submit" event listener to it.
+    //       The event listener should call postHandler above.
+}
+
+export default setup;
