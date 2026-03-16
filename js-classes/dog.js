04bc0130082721ae58cccb299b93910eea639915
Elhamdadi <helhamdadistonehill.edu@F-HELHAMDADI.local>
2026-03-16 15:51:22 -0400

    make a post with classes

--- js-classes/dog.js
+++ js-classes/dog.js
@@ -1,15 +1,43 @@
 class Dog {
+
     constructor(name){
         this.name = name;
     }
 
     bark(){
+        this.secretProperty = "secret";
         console.log("woof");
     }
 
     getName(){
         return this.name;
     }
+
+    setName(name){
+            
+        this.name = name;
+    }
 }
 
+
+// fido.bark();
+// console.log(fido.getName());
+
+// create another Dog object and give it a different name
+// const max = new Dog("max");
+// max.bark();
+// console.log(max.getName());
+
+// max.setName("fido");
+// console.log(max.getName());
+
+// console.log(max);
+
+// add a setName method to the Dog class (test it)
+
+// max.name = "max";
+// console.log(max.name)
+
 const fido = new Dog("fido");
+fido.bark();
+console.log(fido);
