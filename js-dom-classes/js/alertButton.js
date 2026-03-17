c962bfa21380d713ac9d215b2f520084cfb5b247
Elhamdadi <helhamdadistonehill.edu@F-HELHAMDADI.local>
2026-03-17 09:22:05 -0400

    end of class code 3/16

--- js-dom-classes/js/alertButton.js
+++ js-dom-classes/js/alertButton.js
@@ -2,17 +2,18 @@ class AlertButton {
     constructor(containerElement, message){
         this.message = message;
 
-        const button = document.createElement("button"); // <button></button>
-        button.classList.add("alert-button"); // <button class="alert-button"></button>
-        button.type = "button"; // <button class="alert-button" type="button"></button>
-        button.textContent = "Alert: " + message;
+        const HTMLButton = document.createElement("button"); // <button></button>
+        HTMLButton.classList.add("alert-button"); // <button class="alert-button"></button>
+        HTMLButton.type = "button"; // <button class="alert-button" type="button"></button>
+        HTMLButton.textContent = "Alert: " + message;
 
         this.sendAlert = this.sendAlert.bind(this);
 
 
-        button.addEventListener("click", this.sendAlert);
+        HTMLButton.addEventListener("click", this.sendAlert);
 
-        containerElement.appendChild(button);
+
+        containerElement.appendChild(HTMLButton);
     }
 
     sendAlert(){
--- make-a-post-with-classes/js/posts.js
+++ make-a-post-with-classes/js/posts.js
@@ -1,5 +1,28 @@
 class SocialMediaPost {
     constructor(parentElement, postText, profilePicURL){
+        // this should have a reference to the parentElement
+        // a reference to the postText
+        // a reference to the profilePicURL
+        this.parentElement = parentElement;
+        this.postText = postText;
+        this.profilePicURL = profilePicURL;
+
+        // create the post div and add it to the parentElement
+        // use this.div
+        this.div = document.createElement("div");
+        this.div.classList.add("post");
+
+        const image = document.createElement("img");
+        image.classList.add("profile-pic");
+        image.src = this.profilePicURL;
+        this.div.appendChild(image);
+
+        const p = document.createElement('p');
+        p.textContent = postText;
+        this.div.appendChild(p);
+
+        this.parentElement.appendChild(this.div);
+
         
     }
 }
